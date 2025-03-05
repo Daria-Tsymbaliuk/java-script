5546275bc133e2549d9b863a930dc0c7fc2a1159
Daria-Tsymbaliuk <dasha14072011@gmail.com>
2025-03-05 18:09:42 +0100

    Add js

---
+++ index.js
@@ -0,0 +1,31 @@
+let age = 13;
+console.log(age)
+
+const myName = "Daria"; 
+console.log(myName)
+
+let isStudent  = true;
+console.log( isStudent  ) 
+
+let myString = "Кожна людина — це зірка, що сяє на своєму власному небі.";
+console.log("myString")
+
+
+let myNumber  =  14;
+myNumber = myNumber + 10;
+console.log(myNumber)
+
+
+let myNull = null;
+console.log(myNull)
+
+let UserName = prompt("Hello what is your name?");
+console.log(UserName)
+alert(" Hi " + UserName + " nice to meet you 😊");
+
+let userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log(userAnswer);
+
+
+alert("Увага! Дія може бути небезпечною.");
+confirm("Ви впевнені, що хочете продовжити?");
