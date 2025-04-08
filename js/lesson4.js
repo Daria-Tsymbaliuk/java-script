ac986f47e70b209140ecb4cb7f8083b33276ff00
Daria-Tsymbaliuk <dasha14072011@gmail.com>
2025-04-08 15:35:11 +0200

    Виправляє код

--- js/lesson4.js
+++ js/lesson4.js
@@ -3,12 +3,8 @@
 let firstText = prompt("Привіт, напиши щось");
 let secondText = prompt("Знову привіт, напиши щось");
 
+alert(firstText && secondText ? "Обидва поля заповнені" : "Не всі поля заповнені");
 
-if (firstText && secondText) {
-    alert("Обидва поля заповнені");
-} else {
-    alert("Не всі поля заповнені");
-}
 
 
 // Завдання 2
@@ -18,11 +14,8 @@ let secondNumber = parseFloat(prompt("Введіть друге число:"));
 
 let result = firstNumber + secondNumber;
 
-if (result > 10) {
-    alert("Сума більша за 10");
-} else {
-    alert("Сума менша або дорівнює 10");
-}
+alert(result > 10 ? "Сума більша за 10" : "Сума менша або дорівнює 10");
+
 
 
 // Завдання 3
@@ -30,31 +23,19 @@ if (result > 10) {
 let text = "Hello world 👽";
 console.log("text :" ,  text)
 console.log( "includes  JavaScript ? ", text.includes("JavaScript"))
+console.log(text.includes("JavaScript") ? "Текст містить слово JavaScript" : "Текст не містить слово JavaScript");
 
-if (text.includes("JavaScript")) {
-    console.log("Текст містить слово JavaScript");
-} else {
-    console.log("Текст не містить слово JavaScript");
-}
 
 // Завдання 4
 
 let number = parseFloat(prompt("Введіть число:"));
-
-if (number > 10 && number < 20) {
-    alert("Число входить в діапазон від 10 до 20");
-} else {
-    alert("Число не входить в діапазон від 10 до 20");
-}
+alert(number > 10 && number < 20 ? "Число входить в діапазон від 10 до 20" : "Число не входить в діапазон від 10 до 20");
 
 
 
 let firstName = prompt("Введіть ім'я:");
 let email = prompt("Введіть email:");
 let password = prompt("Введіть пароль:");
+alert(firstName.length >= 3 && email.includes("@") && email.includes(".") && email.indexOf(".") > email.indexOf("@") && password.length >= 6 ? "✅✅Перенаправлення на іншу сторінку" : "❗❗Помилка: неправильне заповнення");
 
-if ( firstName.length >= 3 && email.includes("@") && email.includes(".", email.indexOf("@")) && password.length >= 6) {
-    alert("✅✅Перенаправлення на іншу сторінку");
-} else {
-    alert("❗❗Помилка: неправильне заповнення");
-}
+
