95251e857fa75670b6fec5cc0fe91a6a58aee319
Daria-Tsymbaliuk <dasha14072011@gmail.com>
2025-04-17 13:10:28 +0200

    Додає lesson6

---
+++ js/lesson6.js
@@ -0,0 +1,47 @@
+
+// Завдання 1
+let i = 1;
+while (i <= 10) {
+  console.log(" Завдання 1 :" , i);
+  i += 1;
+}
+
+// Завдання 2
+for (let i = 2; i <= 20; i += 1) {
+    if (i % 2 !== 0) {
+      continue;
+    }
+    console.log(" Завдання 2 :" ,i);
+  }
+  
+
+// Завдання 3
+for (let i = 1; i <= 10; i += 1) {
+    console.log(`7 x ${i} = ${7 * i}`);
+  }
+  
+
+// Завдання 4
+let n = 15;
+for (let i = 1; ; i += 1) {
+  if (i >= n) {
+    break;
+  }
+  console.log(" Завдання 4 :" ,i);
+}
+
+
+// Завдання 5
+let i1 = 1;
+while (i1 <= 20) {
+  if (i1 % 3 === 0) {
+    i1 += 1;
+    continue;
+  }
+  console.log(" Завдання 55 :" ,i1);
+  i1 += 1;
+}
+
+
+
+
